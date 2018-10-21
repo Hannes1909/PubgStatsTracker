11a80e4d4ea8491a63b3c846fc8118ca3958de5d
Johannes Schön <hannes1909@live.de>
2018-10-21 13:03:41 +0200

    Test 1

--- GetPubgStats/GetPubgStats/Program.cs
+++ GetPubgStats/GetPubgStats/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Http;
 
 namespace GetPubgStats
 {
@@ -6,7 +7,27 @@ namespace GetPubgStats
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var pubgStatsHTTP = new PubgStatsHTTP();
+
+            string apikey = PubgStatsHelper.GetRandomApiKey();
+            string accountid = "account.2b1f61ca3b3448b1b9b06aa4ab2338c6";
+            string seasonid = "division.bro.official.2018-09";
+            string baseUrl = "https://api.pubg.com/shards/pc-eu/players/" + accountid + "/seasons/" + seasonid;
+
+            HttpClientHandler clientHandler = pubgStatsHTTP.ClientHandler;
+            HttpRequestMessage buildRequest = pubgStatsHTTP.BuildRequest(baseUrl, apikey);
+
+            using (HttpClient client = new HttpClient(clientHandler))
+            {
+                var response = client.SendAsync(buildRequest).ConfigureAwait(false).GetAwaiter().GetResult();
+                var responseContent = response.Content.ReadAsStringAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+                Console.WriteLine(responseContent);
+            }
+            Console.ReadLine();
         }
+
+        
+
     }
+
 }
---
+++ GetPubgStats/GetPubgStats/PubgStatsHTTP.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace GetPubgStats
+{
+    class PubgStatsHTTP
+    {
+        public HttpClientHandler ClientHandler { get => clientHandler; }
+        public HttpRequestMessage BuildRequest(string url, string apiToken) { return buildRequest(url, apiToken); }
+
+        private readonly HttpClientHandler clientHandler = new HttpClientHandler
+        {
+            AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+        };
+
+        private HttpRequestMessage buildRequest(string url, string apiToken)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", apiToken);
+            request.Headers.Add("Accept-Encoding", "gzip");
+            request.Headers.Add("Accept", "application/vnd.api+json");
+            return request;
+        }
+    }
+}
